20022854991708b2d0aefc06b445aa163fd814ae
Seyed Sattar Lotfi Fatemi <sattar.fatemi@balad.ir>
2024-05-05 17:49:37 +0330

    minor changes

--- repository/trader.js
+++ repository/trader.js
@@ -1,5 +1,22 @@
-class Trader {
+const kafkaConfig = require('../config/kafkaConfig');
 
+
+class Trader {
+    constructor(kafka) {
+        this.kafka = kafka;
+    }
+
+    broadcastNewCoin(coin) {
+        this.kafka.sendMessage(kafkaConfig.topics.COINS, coin.id, coin);
+    }
+
+    broadcastNewCooperationRing(cooperationRing) {
+        this.kafka.sendMessage(kafkaConfig.topics.COOPERATION_RINGS, cooperationRing.id, cooperationRing);
+    }
+
+    sendMessageToTrader(traderId, message) {
+        this.kafka.sendMessageToTrader(traderId, message);
+    }
 }
 
 module.exports = Trader;
