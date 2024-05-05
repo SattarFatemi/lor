0e26ab2ca049c5cf0b40508a2a8e985cf106f5ba
Seyed Sattar Lotfi Fatemi <sattar.fatemi@balad.ir>
2024-05-05 10:40:59 +0330

    minor changes

---
+++ models/CooperationRing.js
@@ -0,0 +1,10 @@
+class CooperationRing {
+    constructor(id, memberCount, weight, investor) {
+        this.id = id;
+        this.memberCount = memberCount;
+        this.weight = weight;
+        this.investor = investor;
+    }
+}
+
+module.exports = CooperationRing;
